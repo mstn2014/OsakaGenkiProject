f01f4ef2e48b5d84532a0935a8283e1c52e41faf
Ito <allez.sans.question@gmail.com>
2014-10-26 04:09:51 +0900

    [追加]game3のサンプル追加 Ito>Scenes>kawamotosample>game3sample

---
+++ Assets/Users/Ito/Scenes/kawamotosample/charactor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 適当に書いてるので綺麗に書き直してちょ
+public class charactor : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        // ライトの位置まで線形補間
+        iTween.MoveTo(this.gameObject, GameObject.Find("light").transform.position, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        // ライトに触れたら自身を消す
+        Destroy(this.gameObject);
+    }
+}
