fcfc97cc54f642005268a0a71177f449a2df53d8
Kawamoto <tekira1211@yahoo.co.jp>
2014-10-26 09:53:01 +0900

    [更新]KawamotoTest

--- Assets/Users/Kawamoto/Script/GameMain.cs
+++ Assets/Users/Kawamoto/Script/GameMain.cs
@@ -7,20 +7,27 @@ public class GameMain : MonoBehaviour {
 	GameObject BlueChar_clone;
 	GameObject GreenChar_clone;
 	GameObject YerrowChar_clone;
-	
+
+	// コンポーネント用
+	Char_sp	m_Char_sp;
+
+	// 乱数範囲
+	int min = 1;
+	int max = 5;
 
 	// Use this for initialization
 	void Start () {
 
-		// クローンロード
-		RedChar_clone = Resources.Load<GameObject> ("Kawamoto/RedChar");
-		BlueChar_clone = Resources.Load<GameObject> ("Kawamoto/BlueChar");
-		GreenChar_clone = Resources.Load<GameObject> ("Kawamoto/GreenChar");
-		YerrowChar_clone = Resources.Load<GameObject> ("Kawamoto/YerrowChar");
+			// クローンロード
+			RedChar_clone = Resources.Load<GameObject> ("Kawamoto/RedChar");
+			BlueChar_clone = Resources.Load<GameObject> ("Kawamoto/BlueChar");
+			GreenChar_clone = Resources.Load<GameObject> ("Kawamoto/GreenChar");
+			YerrowChar_clone = Resources.Load<GameObject> ("Kawamoto/YerrowChar");
 
-		// 最初のターゲットを選択
-		//CharMoveOrder ();
-		GameObject.Find("RedChar").SendMessage("OnGo");
+			// 最初のターゲットを選択
+			//CharMoveOrder ();
+			m_Char_sp = GameObject.Find ("RedChar").GetComponent<Char_sp> ();
+			m_Char_sp.SetFig = true;
 	}
 	
 	// Update is called once per frame
@@ -31,30 +38,30 @@ public class GameMain : MonoBehaviour {
 	// スポットライトに行くターゲットを選択
 	public void CharMoveOrder(){
 		// コード(1～4の乱数)
-		int Cord = Random.Range (1,5);
+		int Cord = Random.Range (min,max);
 
 		switch (Cord) {
 			case 1:
-				//GameObject.Find("RedChar").SendMessage("OnGo");
-				GameObject.Find("RedChar(Clone)").SendMessage("OnGo");
+				m_Char_sp = GameObject.Find("RedChar(Clone)").GetComponent<Char_sp>();
+				m_Char_sp.SetFig = true;
 				break;
 
 			case 2:
-				//GameObject.Find("RedChar").SendMessage("OnGo");
-				GameObject.Find("RedChar(Clone)").SendMessage("OnGo");
-				//GameObject.Find("BlueChar(Clone)").SendMessage("OnGo");
+				m_Char_sp = GameObject.Find("RedChar(Clone)").GetComponent<Char_sp>();
+				//m_Char_sp = GameObject.Find("BlueChar(Clone)").GetComponent<Char_sp>();
+				m_Char_sp.SetFig = true;
 				break;
 
 			case 3:
-				//GameObject.Find("RedChar").SendMessage("OnGo");
-				GameObject.Find("RedChar(Clone)").SendMessage("OnGo");
-				//GameObject.Find("GreenChar(Clone)").SendMessage("OnGo");
+				m_Char_sp = GameObject.Find("RedChar(Clone)").GetComponent<Char_sp>();
+				//m_Char_sp = GameObject.Find("GreenChar(Clone)").GetComponent<Char_sp>();
+				m_Char_sp.SetFig = true;
 				break;
 
 			case 4:
-				//GameObject.Find("RedChar").SendMessage("OnGo");
-				GameObject.Find("RedChar(Clone)").SendMessage("OnGo");
-				//GameObject.Find("YerrowChar(Clone)").SendMessage("OnGo");
+				m_Char_sp = GameObject.Find("RedChar(Clone)").GetComponent<Char_sp>();
+				//m_Char_sp = GameObject.Find("YerrowChar(Clone)").GetComponent<Char_sp>();
+				m_Char_sp.SetFig = true;
 				break;
 		}
 	}
@@ -62,7 +69,7 @@ public class GameMain : MonoBehaviour {
 	// キャラをランダムで生成する
 	public void NewModelMake(){
 		// コード(1～4の乱数)
-		int Cord = Random.Range (1,5);
+		int Cord = Random.Range (min,max);
 		int Lng = Random.Range (1,5);
 
 		switch (Cord) {
@@ -74,29 +81,33 @@ public class GameMain : MonoBehaviour {
 			case 2:
 			// 新たなモデルを生成
 				RedChar_clone = Instantiate (RedChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
-			//Instantiate (BlueChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity);	// 新たなモデルを生成
+				//BlueChar_clone = Instantiate (BlueChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
 				break;
 				
 			case 3:
 			// 新たなモデルを生成
 				RedChar_clone = Instantiate (RedChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
-			//Instantiate (GreenChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity);	// 新たなモデルを生成
+				//GreenChar_clone = Instantiate (GreenChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
 				break;
 				
 			case 4:
 			// 新たなモデルを生成
 				RedChar_clone = Instantiate (RedChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
-			//Instantiate (YerrowChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity);// 新たなモデルを生成
+				//YerrowChar_clone = Instantiate (YerrowChar_clone, new Vector3 (20, 0, 5+Lng), Quaternion.identity) as GameObject;
 				break;
 		}
 	}
 
 	//　削除開始
-	public void ModelDelete(GameObject obj){ Destroy (obj.gameObject); }
+	public void ModelDelete(GameObject obj){
+		Destroy (obj.gameObject); 
+		NewModelMake ();
+	}
 
 	// 削除と選定
 	public void ModelDeleteOrder(GameObject obj){
 		Destroy (obj.gameObject);
+		NewModelMake ();
 		CharMoveOrder ();
 	}
 }
--- Assets/Users/Kawamoto/Script/Hit.cs
+++ Assets/Users/Kawamoto/Script/Hit.cs
@@ -26,12 +26,29 @@ public class Hit : MonoBehaviour {
 	
 	}
 
-
+	// ボタンを押したときの処理
 	private void OnTriggerStay(Collider other)
 	{
 		if (other.gameObject.name == "RedChar(Clone)" || other.gameObject.name == "RedChar") {
 			if (input_bt.RedButtonTrigger){ 
-				gamemain.NewModelMake();// モデルを生成
+				gamemain.ModelDeleteOrder(other.gameObject);// モデルの削除＆新しいターゲットの選定
+			}
+		}
+
+		if (other.gameObject.name == "BlueChar(Clone)" || other.gameObject.name == "BlueChar") {
+			if (input_bt.BlueButtonTrigger){ 
+				gamemain.ModelDeleteOrder(other.gameObject);// モデルの削除＆新しいターゲットの選定
+			}
+		}
+
+		if (other.gameObject.name == "GreenChar(Clone)" || other.gameObject.name == "GreenChar") {
+			if (input_bt.GreenButtonTrigger){ 
+				gamemain.ModelDeleteOrder(other.gameObject);// モデルの削除＆新しいターゲットの選定
+			}
+		}
+
+		if (other.gameObject.name == "YerrowChar(Clone)" || other.gameObject.name == "YerrowChar") {
+			if (input_bt.YellowButtonTrigger){ 
 				gamemain.ModelDeleteOrder(other.gameObject);// モデルの削除＆新しいターゲットの選定
 			}
 		}
