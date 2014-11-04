fafd88d8173ce1243fd1db04bc4d298bd3f583bf
EMERALDAKA <backaround_sp_finger@yahoo.co.jp>
2014-11-04 21:20:56 +0900

    [その他]途中経過

---
+++ Assets/Users/Shingaki/C#/objectMgr.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMgr : MonoBehaviour {
+	
+	[Header("Object")]
+	public GameObject m_objParent;	// GameObjectの一番上.
+	public GameObject m_gallery;	// ギャラリー.
+	private GameObject m_panel;		// NGUIの親.
+	
+	// Use this for initialization
+	void Start () {
+		m_panel = GameObject.Find ("Panel");
+		//m_objParent = Instantiate (m_objParent);
+		// ギャラリーを人数作る.
+		// ToDo ランダムでキャラのポジション(中心からの距離を重みでばらけるように).
+		//GameObject 
+		// 
+		for(int i=0; i<10; i++){
+			//Instantiate();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
